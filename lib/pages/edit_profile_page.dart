4c6c5912fa92fb526cee961ac09167f32bb913ec
Nyaraih-Tok <nyaraihk@gmail.com>
2025-11-23 18:10:09 +0200

    add styles to the widgets

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -1,1 +1,72 @@
 import 'package:flutter/material.dart';
+
+class EditProfilePage extends StatelessWidget {
+  const EditProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Edit Profile"), centerTitle: true),
+
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Center(
+              child: Stack(
+                children: [
+                  CircleAvatar(
+                    radius: 60,
+                    backgroundColor: Colors.grey.shade300,
+                    child: const Icon(Icons.person, size: 60),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: CircleAvatar(
+                      radius: 20,
+                      backgroundColor: Colors.blue,
+                      child: const Icon(Icons.camera_alt, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Full Name",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            TextField(
+              decoration: InputDecoration(
+                labelText: "Email",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 16),
+
+            TextField(
+              obscureText: true,
+              decoration: InputDecoration(
+                labelText: "Password",
+                border: OutlineInputBorder(),
+              ),
+            ),
+
+            const SizedBox(height: 30),
+
+            ElevatedButton(onPressed: () {}, child: const Text("Save Changes")),
+          ],
+        ),
+      ),
+    );
+  }
+}
